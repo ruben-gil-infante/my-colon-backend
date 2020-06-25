5c5340ce184b50381b8a0d4965923302eb241af2
rgili <rgili@edu.tecnocampus.cat>
2020-06-25 23:00:19 +0200

    Last commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,188 +1,188 @@
-/* TAULA USUARI */
-DROP TABLE IF EXISTS usuari;
+    /* TAULA USUARI */
+    DROP TABLE IF EXISTS usuari;
 
-CREATE TABLE usuari(
-    id                      SERIAL          PRIMARY KEY,
-    nom                     VARCHAR         NOT NULL,
-    primer_cognom           VARCHAR         NOT NULL,
-    segon_cognom            VARCHAR         NOT NULL,
-    correu_electronic       VARCHAR         UNIQUE,
-    genere                  INT             NOT NULL,
-    data_naixement          DATE            NOT NULL,
-    password                VARCHAR         NOT NULL
-);
+    CREATE TABLE usuari(
+        id                      SERIAL          PRIMARY KEY,
+        nom                     VARCHAR         NOT NULL,
+        primer_cognom           VARCHAR         NOT NULL,
+        segon_cognom            VARCHAR         NOT NULL,
+        correu_electronic       VARCHAR         UNIQUE,
+        genere                  INT             NOT NULL,
+        data_naixement          DATE            NOT NULL,
+        password                VARCHAR         NOT NULL
+    );
 
-/* CANVIS BUCALS */
-DROP TABLE IF EXISTS canvis_bucals;
+    /* CANVIS BUCALS */
+    DROP TABLE IF EXISTS canvis_bucals;
 
-CREATE TABLE canvis_bucals (
-    id                      SERIAL          PRIMARY KEY,
-    afirmatiu               BOOLEAN         NOT NULL,
-    simptomes               VARCHAR         NOT NULL,
-    data                    DATE            DEFAULT CURRENT_TIMESTAMP ,
-    usuari                  INT             NOT NULL,
-    CONSTRAINT FK_CANVIS_BUCALS_USER_ID  FOREIGN KEY (usuari)
-    REFERENCES usuari(id)
-);
+    CREATE TABLE canvis_bucals (
+        id                      SERIAL          PRIMARY KEY,
+        afirmatiu               BOOLEAN         NOT NULL,
+        simptomes               VARCHAR         NOT NULL,
+        data                    DATE            DEFAULT CURRENT_TIMESTAMP ,
+        usuari                  INT             NOT NULL,
+        CONSTRAINT FK_CANVIS_BUCALS_USER_ID  FOREIGN KEY (usuari)
+        REFERENCES usuari(id)
+    );
 
-/* DIARREES */
-DROP TABLE IF EXISTS diarrees;
+    /* DIARREES */
+    DROP TABLE IF EXISTS diarrees;
 
-CREATE TABLE diarrees (
-    id                  SERIAL          PRIMARY KEY,
-    afirmatiu           BOOLEAN         NOT NULL,
-    vegades             INT             NOT NULL,
-    escala_bristol      INT             NOT NULL,
-    color               INT             NOT NULL,
-    data                DATE            DEFAULT CURRENT_TIMESTAMP ,
-    usuari              INT             NOT NULL,
-    CONSTRAINT FK_DIARRES_USUARI_ID FOREIGN KEY (usuari)
-    REFERENCES usuari (id)
-);
+    CREATE TABLE diarrees (
+        id                  SERIAL          PRIMARY KEY,
+        afirmatiu           BOOLEAN         NOT NULL,
+        vegades             INT             NOT NULL,
+        escala_bristol      INT             NOT NULL,
+        color               INT             NOT NULL,
+        data                DATE            DEFAULT CURRENT_TIMESTAMP ,
+        usuari              INT             NOT NULL,
+        CONSTRAINT FK_DIARRES_USUARI_ID FOREIGN KEY (usuari)
+        REFERENCES usuari (id)
+    );
 
-/* DOLOR */
-DROP TABLE IF EXISTS dolor;
+    /* DOLOR */
+    DROP TABLE IF EXISTS dolor;
 
-CREATE TABLE dolor (
-    id              SERIAL          PRIMARY KEY,
-    afirmatiu       BOOLEAN         NOT NULL,
-    localitzacio    VARCHAR         NOT NULL,
-    valor           INT             NOT NULL,
-    descripcio      VARCHAR         NOT NULL,
-    aparicio        VARCHAR         NOT NULL,
-    data            DATE            DEFAULT CURRENT_TIMESTAMP ,
-    usuari          INT             NOT NULL,
-    CONSTRAINT FK_DOLOR_USUARI_ID     FOREIGN KEY(usuari)
-    REFERENCES usuari(id)
+    CREATE TABLE dolor (
+        id              SERIAL          PRIMARY KEY,
+        afirmatiu       BOOLEAN         NOT NULL,
+        localitzacio    VARCHAR         NOT NULL,
+        valor           INT             NOT NULL,
+        descripcio      VARCHAR         NOT NULL,
+        aparicio        VARCHAR         NOT NULL,
+        data            DATE            DEFAULT CURRENT_TIMESTAMP ,
+        usuari          INT             NOT NULL,
+        CONSTRAINT FK_DOLOR_USUARI_ID     FOREIGN KEY(usuari)
+        REFERENCES usuari(id)
 
-);
+    );
 
-/* FATIGA */
-DROP TABLE IF EXISTS fatiga;
+    /* FATIGA */
+    DROP TABLE IF EXISTS fatiga;
 
-CREATE TABLE fatiga (
-    id              SERIAL      PRIMARY KEY,
-    afirmatiu       BOOLEAN     NOT NULL,
-    puntuacio       INT         NOT NULL,
-    data            DATE        DEFAULT CURRENT_TIMESTAMP ,
-    usuari          INT         NOT NULL,
-    CONSTRAINT FK_FATIGA_USUARI_ID  FOREIGN KEY(usuari)
-    REFERENCES usuari(id)
-);
+    CREATE TABLE fatiga (
+        id              SERIAL      PRIMARY KEY,
+        afirmatiu       BOOLEAN     NOT NULL,
+        puntuacio       INT         NOT NULL,
+        data            DATE        DEFAULT CURRENT_TIMESTAMP ,
+        usuari          INT         NOT NULL,
+        CONSTRAINT FK_FATIGA_USUARI_ID  FOREIGN KEY(usuari)
+        REFERENCES usuari(id)
+    );
 
-/* REESTRENYIMENT */
-DROP TABLE IF EXISTS restrenyiment;
+    /* REESTRENYIMENT */
+    DROP TABLE IF EXISTS restrenyiment;
 
-CREATE TABLE restrenyiment (
-    id                      SERIAL      PRIMARY KEY,
-    afirmatiu               BOOLEAN     NOT NULL,
-    ultim_cop               VARCHAR     NOT NULL,
-    color                   INT         NOT NULL,
-    escala_bristol          INT         NOT NULL,
-    data                    DATE        DEFAULT CURRENT_TIMESTAMP ,
-    usuari                  INT         NOT NULL,
-    CONSTRAINT FK_REESTRENYIMENT_USUARI_ID FOREIGN KEY (usuari)
-    REFERENCES USUARI (id)
-);
+    CREATE TABLE restrenyiment (
+        id                      SERIAL      PRIMARY KEY,
+        afirmatiu               BOOLEAN     NOT NULL,
+        ultim_cop               VARCHAR     NOT NULL,
+        color                   INT         NOT NULL,
+        escala_bristol          INT         NOT NULL,
+        data                    DATE        DEFAULT CURRENT_TIMESTAMP ,
+        usuari                  INT         NOT NULL,
+        CONSTRAINT FK_REESTRENYIMENT_USUARI_ID FOREIGN KEY (usuari)
+        REFERENCES USUARI (id)
+    );
 
-/* NUTRICIO */
-DROP TABLE IF EXISTS nutricio;
+    /* NUTRICIO */
+    DROP TABLE IF EXISTS nutricio;
 
-CREATE TABLE nutricio (
-    id              SERIAL      PRIMARY KEY,
-    esmorzar        INT         NOT NULL,
-    dinar           INT         NOT NULL,
-    berenar         INT         NOT NULL,
-    sopar           INT         NOT NULL,
-    gots_aigua      INT         NOT NULL,
-    data            DATE        DEFAULT CURRENT_TIMESTAMP,
-    usuari          INT         NOT NULL,
-    CONSTRAINT FK_NUTRICIO_USUARI   FOREIGN KEY (usuari)
-    REFERENCES usuari (id)
-);
+    CREATE TABLE nutricio (
+        id              SERIAL      PRIMARY KEY,
+        esmorzar        INT         NOT NULL,
+        dinar           INT         NOT NULL,
+        berenar         INT         NOT NULL,
+        sopar           INT         NOT NULL,
+        gots_aigua      INT         NOT NULL,
+        data            DATE        DEFAULT CURRENT_TIMESTAMP,
+        usuari          INT         NOT NULL,
+        CONSTRAINT FK_NUTRICIO_USUARI   FOREIGN KEY (usuari)
+        REFERENCES usuari (id)
+    );
 
-/* MEDICACIO */
-DROP TABLE IF EXISTS medicacio;
+    /* MEDICACIO */
+    DROP TABLE IF EXISTS medicacio;
 
-CREATE TABLE medicacio (
-    id              SERIAL          PRIMARY KEY,
-    dosi            VARCHAR         NOT NULL,
-    nom             VARCHAR         NOT NULL,
-    forma           INT             NOT NULL,
-    franja          INT             NOT NULL,
-    data            VARCHAR         DEFAULT CURRENT_TIMESTAMP ,
-    usuari          INT             NOT null,
-    CONSTRAINT FK_MEDICACIO_USUARI FOREIGN KEY (usuari)
-    REFERENCES USUARI (id)
-);
+    CREATE TABLE medicacio (
+        id              SERIAL          PRIMARY KEY,
+        dosi            VARCHAR         NOT NULL,
+        nom             VARCHAR         NOT NULL,
+        forma           INT             NOT NULL,
+        franja          INT             NOT NULL,
+        data            VARCHAR         DEFAULT CURRENT_TIMESTAMP ,
+        usuari          INT             NOT null,
+        CONSTRAINT FK_MEDICACIO_USUARI FOREIGN KEY (usuari)
+        REFERENCES USUARI (id)
+    );
 
-/* EVENT */
-DROP TABLE IF EXISTS NOTA;
+    /* EVENT */
+    DROP TABLE IF EXISTS NOTA;
 
-CREATE TABLE nota (
-    id                  SERIAL      PRIMARY KEY,
-    descripcio          VARCHAR     NOT NULL,
-    data                DATE        DEFAULT CURRENT_TIMESTAMP ,
-    usuari              INT         NOT NULL,
-    CONSTRAINT FK_EVENT_USER   FOREIGN KEY (usuari)
-    REFERENCES usuari(id)
-);
+    CREATE TABLE nota (
+        id                  SERIAL      PRIMARY KEY,
+        descripcio          VARCHAR     NOT NULL,
+        data                DATE        DEFAULT CURRENT_TIMESTAMP ,
+        usuari              INT         NOT NULL,
+        CONSTRAINT FK_EVENT_USER   FOREIGN KEY (usuari)
+        REFERENCES usuari(id)
+    );
 
-/* VOMITS */
-DROP TABLE IF EXISTS vomits;
+    /* VOMITS */
+    DROP TABLE IF EXISTS vomits;
 
-CREATE TABLE vomits (
-    id              SERIAL          PRIMARY KEY,
-    afirmatiu       BOOLEAN         NOT NULL,
-    cops            INT             NOT NULL,
-    descripcio      VARCHAR         NOT NULL,
-    avui            BOOLEAN         NOT NULL,
-    data            DATE            DEFAULT CURRENT_TIMESTAMP,
-    usuari          INT             NOT NULL,
-    constraint FK_VOMITS_USUARI foreign key (usuari)
-    references usuari(id)
-);
+    CREATE TABLE vomits (
+        id              SERIAL          PRIMARY KEY,
+        afirmatiu       BOOLEAN         NOT NULL,
+        cops            INT             NOT NULL,
+        descripcio      VARCHAR         NOT NULL,
+        avui            BOOLEAN         NOT NULL,
+        data            DATE            DEFAULT CURRENT_TIMESTAMP,
+        usuari          INT             NOT NULL,
+        constraint FK_VOMITS_USUARI foreign key (usuari)
+        references usuari(id)
+    );
 
-/* ESDEVENIMENT */
-DROP TABLE IF EXISTS esdeveniment;
+    /* ESDEVENIMENT */
+    DROP TABLE IF EXISTS esdeveniment;
 
-CREATE TABLE esdeveniment (
-    id                      SERIAL          PRIMARY KEY,
-    data                    DATE            NOT NULL,
-    numero_telefon          VARCHAR         NOT NULL,
-    ubicacio                VARCHAR         NOT NULL,
-    descripcio              VARCHAR         NOT NULL
-);
-
-
-
-/* SIGNE VITAL */
-DROP TABLE IF EXISTS signe_vital;
-
-CREATE TABLE signe_vital(
-    id                      SERIAL          PRIMARY KEY,
-    data                    DATE            NOT NULL,
-    valor                   INT             NOT NULL,
-    valor_secundari         INT             DEFAULT -1,
-    tipus                   INT             NOT NULL,
-    usuari                  INT             NOT NULL,
-    constraint FK_SIGNE_VITAL_USUARI foreign key (usuari)
-    references usuari(id)
-);
+    CREATE TABLE esdeveniment (
+        id                      SERIAL          PRIMARY KEY,
+        data                    DATE            NOT NULL,
+        numero_telefon          VARCHAR         NOT NULL,
+        ubicacio                VARCHAR         NOT NULL,
+        descripcio              VARCHAR         NOT NULL
+    );
 
 
-/* MESSAGE */
-DROP TABLE IF EXISTS missatge;
 
-CREATE TABLE missatge (
-    id                  SERIAL          PRIMARY KEY,
-    text                VARCHAR         NULL,
-    emisor_id           INT             NOT NULL,
-    receptor_id         INT             NOT NULL,
-    codi_conversa       VARCHAR         NOT NULL,
+    /* SIGNE VITAL */
+    DROP TABLE IF EXISTS signe_vital;
 
-    constraint FK_EMISOR_ID_USUARI foreign key (emisor_id)
-    references usuari(id),
-    constraint FK_RECEPTOR_ID foreign key (receptor_id)
-    references usuari(id)
-);
+    CREATE TABLE signe_vital(
+        id                      SERIAL          PRIMARY KEY,
+        data                    DATE            NOT NULL,
+        valor                   INT             NOT NULL,
+        valor_secundari         INT             DEFAULT -1,
+        tipus                   INT             NOT NULL,
+        usuari                  INT             NOT NULL,
+        constraint FK_SIGNE_VITAL_USUARI foreign key (usuari)
+        references usuari(id)
+    );
+
+
+    /* MESSAGE */
+    DROP TABLE IF EXISTS missatge;
+
+    CREATE TABLE missatge (
+        id                  SERIAL          PRIMARY KEY,
+        text                VARCHAR         NULL,
+        emisor_id           INT             NOT NULL,
+        receptor_id         INT             NOT NULL,
+        codi_conversa       VARCHAR         NOT NULL,
+
+        constraint FK_EMISOR_ID_USUARI foreign key (emisor_id)
+        references usuari(id),
+        constraint FK_RECEPTOR_ID foreign key (receptor_id)
+        references usuari(id)
+    );
